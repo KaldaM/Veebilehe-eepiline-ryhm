b579e92bb67060a5588b63a251a1393f72c43d92
KaldaM <matteuskalda@gmail.com>
2024-11-09 16:24:35 +0200

    Lisasin jooke sõnastikku

--- isikukoodi veebileht.py
+++ isikukoodi veebileht.py
@@ -17,7 +17,7 @@ def info_isikukoodist(isikukood_str):
 
     kuu_nr = str(isikukood_list[3]) + str(isikukood_list[4])
     paeva_nr = str(isikukood_list[5]) + str(isikukood_list[6])
-    date_string = (f'{aasta}-{kuu_nr}-{paeva_nr}')
+    date_string = f'{aasta}-{kuu_nr}-{paeva_nr}'
     synni_kuupaev = datetime.strptime(date_string, '%Y-%m-%d').date()
     praegune_kuupaev = datetime.now().date()
 
@@ -70,17 +70,17 @@ def on_korrektne_isikukood(isikukood_str):
 def soovitatud_joogid(sugu, vanus, haigla):
      sonastik = {
          ('mees', 'Kuressaare haigla'): {
-             'noor': ['Segumahl', 'Saaremaa tuulik', 'Viin Saaremaa'],
+             'noor': ['Segumahl', 'Saaremaa tuulik', 'Hlibny Dar'],
              'keskiga': ['Must kohv', 'Rock', 'Viin Saaremaa'],
-             'vana': ['Mineraalvesi', 'Balletine\'s viski', 'Viin Saaremaa']
+             'vana': ['Saaremaa vesi', 'Vana Tallinn', 'Viin Saaremaa']
          },
          ('naine', 'Kuressaare haigla'): {
-             'noor': ['Õunamahl','Sommersby siider', 'Saaremaa Gin'],
+             'noor': ['Õunamahl','Somersby siider', 'Saaremaa Gin'],
              'keskiga': ['Kohv piimaga', 'Punane vein', 'Saaremaa Gin'],
-             'vana': ['Mineraalvesi', 'Punane vein', 'Viin Saaremaa']
+             'vana': ['Saaremaa vesi', 'Punane vein', 'Viin Saaremaa']
          },
          ('mees', 'Tartu Ülikooli Naistekliinik'): {
-             'noor': ['Juissi', 'A.Le.Coq Premium', 'Laua Viin', 'Sparta'],
+             'noor': ['Juissi', 'A.Le.Coq Premium', 'Laua Viin'],
              'keskiga': ['Alkoholivaba Premium', 'Alexander', 'Absolut Vodka'],
              'vana': ['Kraanivesi', 'Alexander', 'Valge venelane']
          },
@@ -89,7 +89,136 @@ def soovitatud_joogid(sugu, vanus, haigla):
              'keskiga': ['Must kohv', 'Valge vein', 'Valge rumm'],
              'vana': ['Mullivesi', 'Punane vein', 'midagi head ja kanget']
          },
-         # Siia tulevad ylejaanud haiglad ka veel
+         ('mees', 'Ida-Tallinna keskhaigla, Pelgulinna sünnitusmaja (Tallinn)'): {
+             'noor': ['Banaanismuuti', 'Jägermeister', 'Absolut Vodka'],
+             'keskiga': ['Coca-Cola', 'Tullamore Dew', 'Saku Original'],
+             'vana': ['Must tee', 'Vana Tallinn', 'Viru Valge']
+         },
+         ('naine', 'Ida-Tallinna keskhaigla, Pelgulinna sünnitusmaja (Tallinn)'): {
+             'noor': ['Sprite', 'Captain Morgan', 'Longero'],
+             'keskiga': ['Kali', 'Longero', 'Kingsmill'],
+             'vana': ['Roheline tee', 'Somersby siider', 'Hennessy']
+         },
+         ('mees', 'Keila haigla'): {
+             'noor': ['Limpa limonaad', 'Beloff viin', 'Sparta'],
+             'keskiga': ['Segumahl', 'Captain Morgan', 'Absolut Vodka'],
+             'vana': ['Piim', 'Aboslut vodka', 'Laua viin']
+         },
+         ('naine', 'Keila haigla'): {
+            'noor': ['Red Bull', 'Saaremaa Gin', 'Somersby siider'],
+            'keskiga': ['Kohv piimaga', 'Heineken', 'Saaremaa Gin'],
+            'vana': ['Keefir', 'Hennessy', 'Punane vein']
+         },
+         ('mees', 'Rapla haigla, Loksa haigla, Hiiumaa haigla (Kärdla)'): {
+             'noor': ['Fanta', 'Sparta', 'Jägermeister'],
+             'keskiga': ['Tomatimahl', 'Tullamore Dew', 'Rock'],
+             'vana': ['Mineraalvesi', 'A. Le Coq Premium', 'Laua Viin']
+         },
+         ('naine', 'Rapla haigla, Loksa haigla, Hiiumaa haigla (Kärdla)'): {
+             'noor': ['Pepsi', 'Longero', 'Captain Morgan'],
+             'keskiga': ['Mullivesi', 'Corona', '???'],
+             'vana': ['Kali', '???', '???']
+         },
+         ('mees', 'Ida-Viru keskhaigla (Kohtla-Järve, endine Jõhvi)'): {
+             'noor': ['Ananassimahl', 'Laua Viin', 'Viru Valge'],
+             'keskiga': ['Marjasmuuti', 'Alexander', 'Viru Valge'],
+             'vana': ['Piim', 'Bock', 'Viru Valge']
+         },
+         ('naine', 'Ida-Viru keskhaigla (Kohtla-Järve, endine Jõhvi)'): {
+             'noor': ['Segumahl', 'Cooler', 'Mojito'],
+             'keskiga': ['Jääkohv', 'Kingsmill', 'Valge vein'],
+             'vana': ['Õunamahl', '???', '???']
+         },
+         ('mees', 'Maarjamõisa kliinikum (Tartu), Jõgeva haigla'): {
+             'noor': ['Fanta', '???', '???'],
+             'keskiga': ['Kohv piimaga', '???', '???'],
+             'vana': ['Kraanivesi', '???', '???']
+         },
+         ('naine', 'Maarjamõisa kliinikum (Tartu), Jõgeva haigla'): {
+             'noor': ['Kakao', '???', '???'],
+             'keskiga': ['Kohv', 'Punane vein', '???'],
+             'vana': ['Piim', '???', '???']
+         },
+         ('mees', 'Narva haigla'): {
+             'noor': ['Coca-Cola', '???', '???'],
+             'keskiga': ['Kali', '???', '???'],
+             'vana': ['Mullivesi', '???', '???']
+         },
+         ('naine', 'Narva haigla'): {
+             'noor': ['Limonaad', '???', '???'],
+             'keskiga': ['Must tee', '???', '???'],
+             'vana': ['Banaanismuuti', '???', '???']
+         },
+         ('mees', 'Pärnu haigla'): {
+             'noor': ['Red Bull', '???', '???'],
+             'keskiga': ['Keefir', '???', '???'],
+             'vana': ['Marjasmuuti', '???', '???']
+         },
+         ('naine', 'Pärnu haigla'): {
+             'noor': ['Kraanivesi', '???', '???'],
+             'keskiga': ['Piim', 'Punane vein', '???'],
+             'vana': ['Roheline tee', '???', '???']
+         },
+         ('mees', 'Haapsalu haigla'): {
+             'noor': ['Kali', 'Saku on Ice', 'Mernaya viin'],
+             'keskiga': ['Kraanivesi', 'Rock', 'Viru Valge'],
+             'vana': ['Kohv piimaga', 'Rock', 'Balletine\'s viski']
+         },
+         ('naine', 'Haapsalu haigla'): {
+             'noor': ['Sprite', '???', '???'],
+             'keskiga': ['Kohv piimaga', '???', '???'],
+             'vana': ['Must tee', '???', '???']
+         },
+         ('mees', 'Järvamaa haigla (Paide)'): {
+             'noor': ['Coca-Cola', '???', '???'],
+             'keskiga': ['Saku Original alkoholivaba', '???', '???'],
+             'vana': ['Proteiinijook', '???', '???']
+         },
+         ('naine', 'Järvamaa haigla (Paide)'): {
+             'noor': ['Jääkohv', 'Longero', 'Original Hartwall gin'],
+             'keskiga': ['Piim', 'Jameson viski', 'Punane vein'],
+             'vana': ['Mullivesi', 'Vana Tallinn', 'Koch Tulivesi viin']
+         },
+         ('mees', 'Rakvere haigla, Tapa haigla'): {
+             'noor': ['Claytons', 'Somersby siider', 'Saku On Ice ploom õlu'],
+             'keskiga': ['Falima Ship brändi', 'Alexander', 'Absolut Vodka'],
+             'vana': ['Keefir', 'Valge venelane', 'Viin Saaremaa']
+         },
+         ('naine', 'Rakvere haigla, Tapa haigla'): {
+             'noor': ['Ananassimahl', 'Mojito', 'Heineken'],
+             'keskiga': ['Kohv piimaga', 'Valge vein', 'Martini'],
+             'vana': ['Piim', 'Punane vein', 'Viin Saaremaa']
+         },
+         ('mees', 'Valga haigla'): {
+             'noor': ['Pepsi', 'A.Le Coq G:N Grapefruit', 'Saaremaa Gin'],
+             'keskiga': ['Sierra Tequila Antiguo Plata', 'Black Velvet viski', 'Keefir'],
+             'vana': ['Roheline tee', 'Absolut Vodka', 'Saku kuld']
+         },
+         ('naine', 'Valga haigla'): {
+             'noor': ['Jäätee', 'Red Bull', 'Saku Originaal'],
+             'keskiga': ['Fizz Blueberry siider', 'Zubrowka Biala viin', 'Angel Medium Sweet White vahuvein'],
+             'vana': ['Mullivesi', 'Mozart Dark Chocolate', 'Southern Road Cabernet Sauvignon  vein']
+         },
+         ('mees', 'Viljandi haigla'): {
+             'noor': ['Piim', 'Monster energiajook', 'Laua Viin'],
+             'keskiga': ['Corona', 'Heineken', 'Falima Ship brändi'],
+             'vana': ['Banaanismuuti', 'Piim', 'Kakao']
+         },
+         ('naine', 'Viljandi haigla'): {
+             'noor': ['Aura multinektar', 'Kohvi', 'Original Hartwall Long Drink sidrun'],
+             'keskiga': ['Viticcio Monile vein', 'Hoggys Raspberry Dream siider', 'Mozart Chocolate liköör'],
+             'vana': ['Kraanivesi', 'Proteiinijook', 'Keefir']
+         },
+         ('mees', 'Lõuna-Eesti haigla (Võru), Põlva haigla'): {
+             'noor': ['Limonaad', 'Saku On Ice ploom õlu', 'Red Bull'],
+             'keskiga': ['Budweiser', 'Casino', 'A. Le Coq Premium'],
+             'vana': ['Vana Tallinn', 'Alexander', 'Must tee']
+         },
+         ('naine', 'Lõuna-Eesti haigla (Võru), Põlva haigla'): {
+             'noor': ['Sprite', 'Long Drink Gin-Pineapple', 'Cloudy Vintage siider'],
+             'keskiga': ['Monkey Gland', 'Martini', 'Karl Friedrich'],
+             'vana': ['Piim', 'Roheline tee', 'Absolut Vodka']
+         },
          ('mees', 'Sünnitushaigla ei ole teada'): {
              'noor': ['!', '@', '#'],
              'keskiga': ['$', '%', '^'],
